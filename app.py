8a17783d0698fe4d45ff24e82377779eadcd6823
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-07 11:27:09 +0300

    Add Chart inside the excel

--- app.py
+++ app.py
@@ -482,6 +482,7 @@ path = Path("emails")
     Automation with python 
 """
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook('transactions.xlsx')
 
@@ -504,6 +505,14 @@ for row in range(2, sheet.max_row +1):
     corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row, 4) 
     corrected_price_cell.value = corrected_price
+
+values = Reference(sheet, min_row=2, max_row=sheet.
+                   max_row, min_col=4, max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+
+sheet.add_chart(chart, 'e2')
 
 print(wb.save('transactions2.xlsx'))
 
