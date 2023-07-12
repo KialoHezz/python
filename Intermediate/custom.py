c65086236da00447e8b94ace08cc9c893ef96e22
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-12 11:33:03 +0300

    logger

--- Intermediate/custom.py
+++ Intermediate/custom.py
@@ -4,5 +4,33 @@ import logging
     1. import logging
 """
 
-logger = logging.getLogger(__name__) #create logger with the filename as I have name the file
-logger.info('Hello from custom')
+# logger = logging.getLogger(__name__) #create logger with the filename as I have name the file
+# logger.propagate = False
+# logger.info('Hello from custom')
+
+"""
+    Lock handler.
+    => Handler objects are responsible for dispatching the appropriate lock message to the handlers specific destination, for example you can use lock handler to send log messages to this standard output stream to files vis HTTPL or via via email
+"""
+# set up 
+logger = logging.getLogger(__name__)
+
+# create handler
+
+stream_h = logging.StreamHandler()
+file_h = logging.FileHandler('file.log')
+
+# Set the level and the format
+stream_h.setLevel(logging.WARNING)
+file_h.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+
+logger.warning('This is a Warning')
+logger.error('This is an error')
