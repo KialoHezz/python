6e53b0746da879a1365405d305e1e83266533ea6
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-13 14:37:33 +0300

    Add generator

--- Intermediate/generatorss.py
+++ Intermediate/generatorss.py
@@ -8,20 +8,20 @@ def mygenerator():
     yield 3
 
 #   create generator object
-g = mygenerator()
+# g = mygenerator()
 
 # # loop
 # for i in g:
 #     print(i)
 
-value = next(g)
-print(value)
+# value = next(g)
+# print(value)
 
-value = next(g)
-print(value)
+# value = next(g)
+# print(value)
 
-value = next(g)
-print(value)
+# value = next(g)
+# print(value)
 
 # Stop looping if doesn't find another yield statement
 # sort()
@@ -39,4 +39,32 @@ cd = countdown(4)
 
 # get first value
 value = next(cd)
-print(value)
+# print(value)
+
+
+"""
+    Adv. of generators
+    1. Memory efficient when you work with large data.
+    2. 
+"""
+
+# 1. Memory efficient when you work with large data. e.g
+import sys
+
+def firstn(n):
+    nums = []
+    num = 0
+    while num < n:
+        nums.append(num)
+        num += 1
+    return nums
+
+# USE GENERATOR
+def firstn_generator(n):
+    num = 0
+    while num < n:
+        yield num
+        num += 1
+
+print(sys.getsizeof(firstn(1000000)))
+print(sys.getsizeof(firstn_generator(1000000)))
