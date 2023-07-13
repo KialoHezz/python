c97d11988b8c68d2769ff12d48cdc90a8fc6ba46
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-13 11:09:53 +0300

    JSON

---
+++ Intermediate/j.py
@@ -0,0 +1,41 @@
+"""
+    Encode and Decode
+
+    =========================================
+    1. Python
+        # dict
+        # list, tuple
+        # str
+        # int, long, float
+        # True
+        # False
+        # None
+
+    2. JSON
+        # object
+        # array
+        # string
+        # number
+        # true
+        # false
+        # null
+    Serialization from python to json
+"""
+import json
+
+# python object into JSON
+person = {
+    "name": "Hezron",
+    "age": 30,
+    "city": "Nairobi",
+    "hasChildren": False,
+    "title": ["Software Developer", "Graphic Designer"]
+}
+
+personJSON = json.dumps(person, indent=4, sort_keys=True)
+print(personJSON)
+
+# open file with mode of write
+with open('person.json', 'w') as file:
+    # dump person object
+    json.dump(person, file, indent=4)
