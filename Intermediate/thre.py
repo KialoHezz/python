f982787454b7d288f49a2540c63dbe84c4b24e62
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-13 16:52:17 +0300

    Data shar with the threads / locks

--- Intermediate/thre.py
+++ Intermediate/thre.py
@@ -1,35 +1,49 @@
-from threading import Thread
-import os
+from threading import Thread, Lock
 import time
+"""
+    Recap : Create & Start a thread 
+            2. Share data btn threads and how to use logs to prevent race condition.
+            3. Learn what's daemon process and how we can use a queue for thread safe data exchange
+"""
 
-def square_numbers():
-    for i in range(100):
-        i * i
+# Share data btn threads
+# Since thread live same memory space
+
+# shld simulate the database
+database_value = 0
+
+def increase(lock):
+    # modify global variable
+    global database_value
+
+    lock.acquire()
+    # get value and store in local copy
+    local_copy = database_value
+    # processing
+    local_copy += 1
+    # time.sleep() => switches to another thread
+    time.sleep(0.1) # race condition happens here, therefore, it happen when two or more threads try to try to modify the same varible at the same time
+
+    # write new value to our DB
+    database_value = local_copy
+    lock.release()
 
 if __name__ == "__main__":
-    threads = []
-    num_threads = 10
 
-# create the list where to store all process
-threads = []
-# no. of CPU on my machine
-num_threads =  10
+    lock = Lock()
+    print('Start value', database_value)
 
-# create processes
-for i in range(num_threads):
-    t = Thread(target=square_numbers)
-    threads.append(t)
+    # create 2 threads:
+    thread1 = Thread(target=increase, args=(lock, ))
+    thread2 = Thread(target=increase, args=(lock, ))
+    
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+    print('end value', database_value)
 
 
-# start the processes
-for t in threads:
-    t.start()
-
-# Join the processes
-# Means wait for a process to finish and while i'm waiting ; i'm blocking main thread
-for t in threads:
-    t.join()
-
-# reach this point all processes are done
-print('end main')
-
+    print('end main')
