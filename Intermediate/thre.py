9182b429dac7ba8865d1e6b20ffff8161896efc5
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-13 17:10:58 +0300

    Queue

--- Intermediate/thre.py
+++ Intermediate/thre.py
@@ -1,49 +1,79 @@
 from threading import Thread, Lock
+from queue import Queue
 import time
+"""
+    Queue is a data structure that follows the First-In-First-Out (FIFO) principle.
+    for EXAMPLE, Queue of customer waiting in a line
+"""
+
+if __name__ == "__main__":
+
+    q = Queue()
+
+    q.put(1)
+    q.put(2)
+    q.put(3)
+
+    # 3 2 1 ------> get the first value
+    first  = q.get()
+    print(first)
+
+    # done the processing
+    q.task_done()
+    # block the main queue
+    q.join()
+
+    # # check queue is empty
+    # q.empty()  
+
+
+    print('end main')
+
+
 """
     Recap : Create & Start a thread 
             2. Share data btn threads and how to use logs to prevent race condition.
             3. Learn what's daemon process and how we can use a queue for thread safe data exchange
 """
 
-# Share data btn threads
-# Since thread live same memory space
+# # Share data btn threads
+# # Since thread live same memory space
 
-# shld simulate the database
-database_value = 0
+# # shld simulate the database
+# database_value = 0
 
-def increase(lock):
-    # modify global variable
-    global database_value
+# def increase(lock):
+#     # modify global variable
+#     global database_value
 
-    with lock:
-        # get value and store in local copy
-        local_copy = database_value
-        # processing
-        local_copy += 1
-        # time.sleep() => switches to another thread
-        time.sleep(0.1) # race condition happens here, therefore, it happen when two or more threads try to try to modify the same varible at the same time
+#     with lock:
+#         # get value and store in local copy
+#         local_copy = database_value
+#         # processing
+#         local_copy += 1
+#         # time.sleep() => switches to another thread
+#         time.sleep(0.1) # race condition happens here, therefore, it happen when two or more threads try to try to modify the same varible at the same time
 
-        # write new value to our DB
-        database_value = local_copy
+#         # write new value to our DB
+#         database_value = local_copy
     
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    lock = Lock()
-    print('Start value', database_value)
+#     lock = Lock()
+#     print('Start value', database_value)
 
-    # create 2 threads:
-    thread1 = Thread(target=increase, args=(lock, ))
-    thread2 = Thread(target=increase, args=(lock, ))
+#     # create 2 threads:
+#     thread1 = Thread(target=increase, args=(lock, ))
+#     thread2 = Thread(target=increase, args=(lock, ))
     
-    thread1.start()
-    thread2.start()
+#     thread1.start()
+#     thread2.start()
 
-    thread1.join()
-    thread2.join()
+#     thread1.join()
+#     thread2.join()
 
-    print('end value', database_value)
+#     print('end value', database_value)
 
 
-    print('end main')
+#     print('end main')
