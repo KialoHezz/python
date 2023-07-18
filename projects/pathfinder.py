3d5be33b50a001e37b2dbda6be918998a30380c0
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-18 10:59:53 +0300

    Init colors

--- projects/pathfinder.py
+++ projects/pathfinder.py
@@ -1,4 +1,5 @@
 # use package called curses, curses => used to control the terminal and colored in the way that you saw and actuall override the current in terminal 
+# Linux $ MAC is already installed, therefore, this command for windows guys want to install curses => pip install windows-curses
 
 # nested loop
 #  O => start
@@ -24,10 +25,15 @@ maze = [
 # dfn function
 # parameter stdscr(standard output screen)
 def main(stdscr):
+    # init color
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    # USE THE COLOR 
+    blue_and_black = curses.color_pair(1)
     stdscr.clear()
     # row & column 0, 0 => top-left-corner = 0 
     # pass the text show on the screen
-    stdscr.addstr(0, 0, "hello world")
+    stdscr.addstr(0, 0, "hello world", blue_and_black)
     stdscr.refresh()
     # allow user to hit sthg
     stdscr.getch()
