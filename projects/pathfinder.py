769361b63fc0acd66647c747007930f3dd3c7605
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-18 11:21:17 +0300

    dfn another function(print_maze), passed in maze, stdscr & path=[], have pair colors(RED=>path & BLUE=> maze) & Loop through each row of maze and also loop through each column in the row

--- projects/pathfinder.py
+++ projects/pathfinder.py
@@ -22,18 +22,36 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    # DRAW MAZE AS BLUE
+    BLUE = curses.color_pair(1)
+    # THE PATH AS RED
+    RED = curses.color_pair(2)
+
+    # LOOP THROUGH
+    #  i => it's row while j => it's row
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, BLUE)
+
+
+
+
 # dfn function
 # parameter stdscr(standard output screen)
 def main(stdscr):
     # init color
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    # USE THE COLOR 
-    blue_and_black = curses.color_pair(1)
+
+
+    # # USE THE COLOR 
+    # blue_and_black = curses.color_pair(1)
+
     stdscr.clear()
     # row & column 0, 0 => top-left-corner = 0 
     # pass the text show on the screen
-    stdscr.addstr(0, 0, "hello world", blue_and_black)
+    print_maze(maze, stdscr)
     stdscr.refresh()
     # allow user to hit sthg
     stdscr.getch()
