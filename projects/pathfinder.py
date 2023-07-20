59d0e3fe5d2527e9cfa649908fa65bef221574f0
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-07-20 14:17:21 +0300

    Finder Path [Breath First Search ALgorithm and Queue the DS]

--- projects/pathfinder.py
+++ projects/pathfinder.py
@@ -13,11 +13,11 @@ import time
 
 maze = [
     ["#", "#", "#", "#", "#", "O", "#", "#", "#"],
-    ["#", " ", " ", " ", "#", " ", " ", " ", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
     ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
     ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
-    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
@@ -32,11 +32,77 @@ def print_maze(maze, stdscr, path=[]):
     #  i => it's row while j => it's row
     for i, row in enumerate(maze):
         for j, value in enumerate(row):
-            stdscr.addstr(i, j*2, value, BLUE)
+            if (i, j) in path:  
+                stdscr.addstr(i, j*2, "X", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
+
+
+# find the co-ordinates
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j 
+    return None
 
 
 
+def find_path(maze, stdscr):
+    # dfn variables
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
 
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
+
+    # keep track of visited nodes
+    visited = set()
+
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
+
+        stdscr.clear()
+        print_maze(maze, stdscr, path)
+        time.sleep(0.2)
+        stdscr.refresh()
+
+        # return that find the path
+        if maze[row][col] == end:
+            return path
+        
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            # check if there is obstace
+            if maze[r][c] == "#":
+                continue
+
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+ 
+
+def find_neighbors(maze, row, col):
+    neighbours = []
+
+    if row > 0: #UPPER
+        neighbours.append((row - 1, col))
+    if row + 1 < len(maze): #Down 
+        neighbours.append((row + 1, col))
+    if col > 0: #LEFT 
+        neighbours.append((row, col - 1))
+    if col + 1 > len(maze[0]): #RIGHT
+        neighbours.append((row, col + 1))
+    
+    return neighbours
+        
 # dfn function
 # parameter stdscr(standard output screen)
 def main(stdscr):
@@ -44,16 +110,9 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-
-    # # USE THE COLOR 
-    # blue_and_black = curses.color_pair(1)
-
-    stdscr.clear()
-    # row & column 0, 0 => top-left-corner = 0 
-    # pass the text show on the screen
-    print_maze(maze, stdscr)
-    stdscr.refresh()
+    find_path(maze, stdscr)
     # allow user to hit sthg
     stdscr.getch()
     
 wrapper(main)
+
